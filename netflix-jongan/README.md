b84815fe3945e5f8c637d1b6a0bef9d22e5e2a54
choijongan <mmm7134@naver.com>
2022-12-02 23:51:21 +0900

    axios 세팅

--- netflix-jongan/README.md
+++ netflix-jongan/README.md
@@ -2,3 +2,11 @@ API호출 3대장
 1. Fetch
 2. Ajax
 3. Axios
+
+fetch보다 axios가 기능이 더 많다.(axios는 라이브러리 형태)
+1. 노드js에서는 fetch를 못씀. 하지만 axios는 백엔드 프론트엔드 둘 다 사용 가능
+2. 타임아웃(api가 몇 초 이상 반응이 없으면 끊음)
+3. baseUrl 앞의 url 세팅해줌
+4. request, response를 보낼때마다 상태보기 가능.(에러를 바로 프린팅해줌. 디버깅하기 수월)
+
+axios는 .http함수
--- netflix-jongan/src/page/Home.js
+++ netflix-jongan/src/page/Home.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { movieAction } from '../redux/action/MovieAction'
 
 const Home = () => {
-  useEffect(()=>{
-    
+  const dispatch = useDispatch()
+  useEffect(()=> {
+    dispatch(movieAction.getMovies())
   },[])
   return (
     <div>Home</div>
