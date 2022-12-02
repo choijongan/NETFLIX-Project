024baa2690f412b2ac311b2ae57a36de95f043f7
choijongan <mmm7134@naver.com>
2022-12-03 00:18:03 +0900

    axios data 불러오기 성공

--- netflix-jongan/src/redux/action/MovieAction.js
+++ netflix-jongan/src/redux/action/MovieAction.js
@@ -1,15 +1,17 @@
 import api from "../api";
 
-
+const API_KEY=process.env.REACT_APP_API_KEY
 function getMovies(){
     return async (dispatch)=> {
-        const popularMovieApi = await api.get('/movie/popular?api_key=<<api_key>>&language=en-US&page=1')
+        const popularMovieApi = await api.get(
+            `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+            )
 
-        // let url = '/movie/popular?api_key=<<api_key>>&language=en-US&page=1';
+        // let url = `/movie/popular?api_key=<<api_key>>&language=en-US&page=1`;
         // let response = await fetch(url);
         // let data = await response.json();
 
-        // let url2 = 'https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1';
+        // let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
     }
 }
 
