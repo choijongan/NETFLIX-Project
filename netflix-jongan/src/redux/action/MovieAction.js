043841b62764f396fe901f63799ed03ec6949463
choijongan <mmm7134@naver.com>
2022-12-05 21:32:15 +0900

    Promise.all 작성

--- netflix-jongan/src/redux/action/MovieAction.js
+++ netflix-jongan/src/redux/action/MovieAction.js
@@ -3,15 +3,26 @@ import api from "../api";
 const API_KEY=process.env.REACT_APP_API_KEY
 function getMovies(){
     return async (dispatch)=> {
-        const popularMovieApi = await api.get(
-            `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        const popularMovieApi =  api.get(
+                `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+            )
+        
+            const topRatedApi =  api.get(
+                `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`  
             )
 
+            const upComingApi =  api.get(
+                `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+            )
+            
+            let data = await Promise.all([popularMovieApi,topRatedApi,upComingApi]) //정보 오기전에 한 번만 await 기다리고, 동시에 진행시켜!
+            console.log('우리가 promise all후에 받는 데이터는?', data)
         // let url = `/movie/popular?api_key=<<api_key>>&language=en-US&page=1`;
         // let response = await fetch(url);
         // let data = await response.json();
 
-        // let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
+        // let url2 = `/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
+        // let url3 = `/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1`;
     }
 }
 
