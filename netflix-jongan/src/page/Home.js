9eca3c7d2cf7bd1106466c7e2a5af36ead634a62
choijongan <mmm7134@naver.com>
2022-12-06 20:47:33 +0900

    베너 조건부 렌더링

--- netflix-jongan/src/page/Home.js
+++ netflix-jongan/src/page/Home.js
@@ -1,14 +1,19 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { movieAction } from '../redux/action/MovieAction'
+import Banner from '../components/Banner'
 
 const Home = () => {
   const dispatch = useDispatch()
+  const { popularMovies, topRatedMovies, upcomingMovies } = useSelector(state=>state.movie)
+  
   useEffect(()=> {
     dispatch(movieAction.getMovies())
   },[])
-  return (
-    <div>Home</div>
+  return ( //조건부 렌더링, popularmovies.results값이 있으면 banner를 보여준다는 뜻.
+    <div> 
+      {popularMovies.results && <Banner movie={popularMovies.results[0]} />} 
+    </div>
   )
 }
 
