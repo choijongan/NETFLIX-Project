d8bd2cdbd094216878a9555be1bf679b648f78c7
choijongan <mmm7134@naver.com>
2022-12-14 22:31:48 +0900

    카드디테일

--- netflix-jongan/src/components/MovieCard.js
+++ netflix-jongan/src/components/MovieCard.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Badge } from 'react-bootstrap'
 
 const MovieCard = ({ item }) => {
   return (
     <div 
     className="card-size"
     style={{backgroundImage:
-        "url("+`https://www.themoviedb.org/t/p/w355_and_h200_multi_faces${item.poster_path}`+")"}}>MovieCard</div>
+        "url("+`https://www.themoviedb.org/t/p/w355_and_h200_multi_faces${item.poster_path}`+")"}}
+        >
+          <div className='overlay'>
+            <h1>{item.title}</h1>
+            <div>
+              {item.genre_ids.map((id) => (
+            <Badge bg="danger">{id}</Badge>))}
+            </div>
+            <div>
+              <span>{item.vote_average}</span>
+              <span>{item.adult?"청불":"Under 18"}</span>
+            </div>
+          </div>
+        </div>
   )
 }
 
