32d688ccdd45f3b14a20df3c754755fd7b975df5
choijongan <mmm7134@naver.com>
2022-12-16 16:59:34 +0900

    장르완성

--- netflix-jongan/src/components/MovieCard.js
+++ netflix-jongan/src/components/MovieCard.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Badge } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 
 const MovieCard = ({ item }) => {
+   const { genreList } = useSelector((state) => state.movie)
   return (
     <div 
     className="card-size"
     style={{backgroundImage:
-        "url("+`https://www.themoviedb.org/t/p/w355_and_h200_multi_faces${item.poster_path}`+")"}}
+        "url("+
+        `https://www.themoviedb.org/t/p/w355_and_h200_multi_faces${item.poster_path}`+
+        ")",
+      }}
         >
           <div className='overlay'>
             <h1>{item.title}</h1>
             <div>
               {item.genre_ids.map((id) => (
-            <Badge bg="danger">{id}</Badge>))}
+            <Badge bg="danger">{genreList.find(item=>item.id==id).name}
+            </Badge>))}
             </div>
             <div>
               <span>{item.vote_average}</span>
